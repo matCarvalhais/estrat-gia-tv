6ae7a3d742a9c54e5543a6564494e288dc200ff1
matCarvalhais <105074212+matCarvalhais@users.noreply.github.com>
2026-03-13 20:07:33 -0300

    Update MainActivity.java

--- app/src/main/java/com/mateus/estrategiatv/MainActivity.java
+++ app/src/main/java/com/mateus/estrategiatv/MainActivity.java
@@ -48,10 +48,6 @@ public class MainActivity extends Activity {
         settings.setDisplayZoomControls(false);
         settings.setMediaPlaybackRequiresUserGesture(false);
         settings.setUserAgentString("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
-
-        // Cache persistente
-        String cachePath = getApplicationContext().getCacheDir().getAbsolutePath();
-        settings.setAppCachePath(cachePath);
         settings.setCacheMode(WebSettings.LOAD_DEFAULT);
 
         webView.setWebViewClient(new WebViewClient() {
